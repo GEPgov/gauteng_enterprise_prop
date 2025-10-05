e55fadd34681e6763e8d9134859eaf8ce9241ab9
GEPgov <vngomane@gep.co.za>
2025-10-05 08:30:17 +0200

    removed some files

--- obj/Debug/net8.0/Gauteng Enterprise Propeller.AssemblyInfo.cs
+++ obj/Debug/net8.0/Gauteng Enterprise Propeller.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Gauteng Enterprise Propeller")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+924f342a07915eeca095612b494f30b448af61e2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Gauteng Enterprise Propeller")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Gauteng Enterprise Propeller")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
