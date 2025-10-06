e187c690c60287d03693c90a1a9bbe73a2082384
GEPgov <vngomane@gep.co.za>
2025-10-06 05:20:28 +0200

    Update navs and removed some contents

--- obj/Release/net8.0/Gauteng Enterprise Propeller.AssemblyInfo.cs
+++ obj/Release/net8.0/Gauteng Enterprise Propeller.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Gauteng Enterprise Propeller")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f64e6a2deb4433823e1bf45d9c62ece001a12406")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74f8a17e0bff2668d2c55ecb902875321b006b95")]
 [assembly: System.Reflection.AssemblyProductAttribute("Gauteng Enterprise Propeller")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Gauteng Enterprise Propeller")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
